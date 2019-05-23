2b46713d6e29ad0462e245e8e0b70633d0eccd71
Kevin Meng <kevin.meng.01@gmail.com>
2019-05-23 18:46:55 -0500

    add one to labels

--- data_utils/data_loader.py
+++ data_utils/data_loader.py
@@ -72,4 +72,4 @@ class DataLoader:
         with open(FLAGS.vocab_path, 'rb') as f:
             vc = [x[0] for x in pickle.load(f)]
 
-        return Dataset([[vc.index(ch) for ch in x[1].split(' ')] for x in data], [int(x[0]) for x in data], FLAGS.random_state)
+        return Dataset([[vc.index(ch) for ch in x[1].split(' ')] for x in data], [int(x[0]) + 1 for x in data], FLAGS.random_state)
