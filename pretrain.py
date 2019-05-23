31642bceb7781e335e26f38af1055d39ed8e2b0e
Kevin Meng <kevin.meng.01@gmail.com>
2019-05-23 11:22:35 -0500

    add training functionality

--- pretrain.py
+++ pretrain.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import math
+import time
 import os
 from data_utils.data_loader import DataLoader
 from models.bdlstm import RecurrentModel
@@ -6,6 +8,90 @@ from flags import FLAGS
 
 x = tf.placeholder(tf.int32, (None, None), name='x')
 y = tf.placeholder(tf.int32, (None,), name='y')
+
+
+def validation_stats(sess, cost, acc, batch_x, batch_y):
+    val_loss = sess.run(
+        cost,
+        feed_dict={
+            x: batch_x,
+            y: batch_y
+        }
+    )
+    val_acc = sess.run(
+        acc,
+        feed_dict={
+            x: batch_x,
+            y: batch_y
+        }
+    )
+
+    return val_loss, val_acc
+
+
+def execute_validation(sess, cost, acc, validation_data):
+    n_batches = math.ceil(float(FLAGS.validation_examples) / float(FLAGS.batch_size))
+    val_loss, val_acc = 0.0, 0.0
+    tot_val_ex = 0
+
+    for batch in range(n_batches):
+        batch_x, batch_y = get_batch(batch, validation_data, ver='validation')
+        loss, ac = validation_stats(sess, cost, acc, batch_x, batch_y)
+        val_loss += loss * len(batch_y)
+        val_acc += ac * len(batch_y)
+        tot_val_ex += len(batch_y)
+
+    val_loss /= tot_val_ex
+    val_acc /= tot_val_ex
+    return 'Val Loss: {:>7.4f} Val Acc: {:>7.4f}% '.format(val_loss, val_acc * 100)
+
+
+def batch_stats(sess, batch_x, batch_y, cost, acc):
+    train_loss = sess.run(
+        cost,
+        feed_dict={
+            x: batch_x,
+            y: batch_y
+        }
+    )
+    train_acc = sess.run(
+        acc,
+        feed_dict={
+            x: batch_x,
+            y: batch_y
+        }
+    )
+
+    return train_loss, train_acc
+
+
+def train_neural_network(sess, optimizer, batch_x, batch_y):
+    sess.run(
+        optimizer,
+        feed_dict={
+            x: batch_x,
+            y: batch_y
+        }
+    )
+
+
+def get_batch(bid, data, ver='train'):
+    batch_x = []
+    batch_y = []
+
+    for i in range(FLAGS.batch_size):
+        idx = bid * FLAGS.batch_size + i
+        if idx >= (FLAGS.train_examples if ver == 'train' else FLAGS.validation_examples):
+            break
+        batch_x.append(data.x[idx])
+        batch_y.append(data.y[idx])
+
+    return batch_x, batch_y
+
+
+def save_model(sess, epoch):
+    saver = tf.train.Saver()
+    saver.save(sess, os.path.join(FLAGS.save_loc, 'stm.ckpt'), global_step=epoch)
 
 
 def main():
@@ -22,8 +108,57 @@ def main():
 
     lstm_model = RecurrentModel()
     logits, cost = lstm_model.construct_model(x, y)
-    print(logits, cost)
     optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate).minimize(cost)
+
+    y_pred = tf.nn.softmax(logits)
+    correct = tf.equal(tf.argmax(y), tf.argmax(y_pred))
+    acc = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        start = time.time()
+
+        for epoch in range(FLAGS.num_epochs):
+
+            n_batches = math.ceil(float(FLAGS.training_examples) / float(FLAGS.batch_size))
+
+            n_samples = 0
+            epoch_loss = 0.0
+            epoch_acc = 0.0
+
+            for i in range(n_batches):
+                batch_x, batch_y = get_batch(i, train_data)
+
+                train_neural_network(sess, optimizer, batch_x, batch_y)
+
+                b_loss, b_acc = batch_stats(sess, batch_x, batch_y, cost, acc)
+                epoch_loss += b_loss
+                epoch_acc += b_acc
+                n_samples += len(batch_y)
+
+            epoch_loss /= n_samples
+            epoch_acc /= n_samples
+
+            if epoch % FLAGS.stat_print_interval == 0:
+                log_string = 'Epoch {:>3} Loss: {:>7.4} Acc: {:>7.4f}% '.format(epoch + 1, epoch_loss,
+                                                                                epoch_acc * 100)
+                if validation_data.get_length() > 0:
+                    log_string += execute_validation(sess, cost, acc, validation_data)
+                log_string += '({:3.3f} sec/epoch)'.format((time.time() - start) / epochs_traversed)
+
+                tf.logging.info(log_string)
+                start = time.time()
+                epochs_traversed = 0
+
+            if epoch % FLAGS.model_save_interval == 0 and epoch != 0:
+                save_model(sess, epoch)
+                tf.logging.info('Model @ epoch {} saved'.format(epoch + 1))
+
+        tf.logging.info('Training complete. Saving final model...')
+        save_model(sess, -1)
+        tf.logging.info('Model saved.')
+
+        sess.close()
 
 
 if __name__ == '__main__':
