6215dcb5a5a04ee178cea0f55d6890f3450e1ea5
Kevin Meng <kevin.meng.01@gmail.com>
2019-05-23 22:28:15 -0500

    clean up flags

--- flags.py
+++ flags.py
@@ -27,32 +27,15 @@ flags.DEFINE_integer('test_examples', None, 'Number of testing examples')
 flags.DEFINE_integer('random_state', 42, 'State of consistent pseudo-randomness')
 
 # Embeddings
-flags.DEFINE_string('w2v_loc', 'data/word2vec/GoogleNews-vectors-negative300.bin',
-  'Location of w2v embeddings')
+flags.DEFINE_string('w2v_loc', 'data/word2vec/GoogleNews-vectors-negative300.bin', 'Location of w2v embeddings')
 flags.DEFINE_bool('transfer_learn_w2v', False, 'Train on top of w2v embeddings')
 
 # Adversarial and virtual adversarial training parameters.
-flags.DEFINE_float('perturb_norm_length', 5.0,
-                   'Norm length of adversarial perturbation to be '
-                   'optimized with validation. '
-                   '5.0 is optimal on IMDB with virtual adversarial training. ')
-
-# Virtual adversarial training parameters
-flags.DEFINE_integer('num_power_iteration', 1, 'The number of power iteration')
-flags.DEFINE_float('small_constant_for_finite_diff', 1e-1,
-                   'Small constant for finite difference method')
+flags.DEFINE_float('perturb_norm_length', 5.0, 'Norm length of adversarial perturbation')
 
 # Parameters for building the graph
-flags.DEFINE_string('adv_training_method', None,
-                    'The flag which specifies training method. '
-                    '""    : non-adversarial training (e.g. for running the '
-                    '        semi-supervised sequence learning model) '
-                    '"rp"  : random perturbation training '
-                    '"at"  : adversarial training '
-                    '"vat" : virtual adversarial training '
-                    '"atvat" : at + vat ')
-flags.DEFINE_float('adv_reg_coeff', 1.0,
-                   'Regularization coefficient of adversarial loss.')
+flags.DEFINE_string('adv_training_method', None, 'How adversarial training is to be undertaken'
+flags.DEFINE_float('adv_reg_coeff', 1.0, 'Regularization coefficient of adversarial loss')
 
 # Output stats
 flags.DEFINE_integer('num_classes', 3, 'Number of classes for classification')
@@ -62,11 +45,9 @@ flags.DEFINE_integer('batch_size', 256, 'Size of the batch.')
 flags.DEFINE_integer('num_timesteps', 100, 'Number of timesteps for BPTT')
 
 # Model architecture
-flags.DEFINE_bool('bidir_lstm', True, 'Whether to build a bidirectional LSTM.')
-# flags.DEFINE_bool('dropout', True, 'Whether to build a bidirectional LSTM.')
+flags.DEFINE_bool('bidir_lstm', False, 'Whether to build a bidirectional LSTM.')
 flags.DEFINE_integer('rnn_num_layers', 1, 'Number of LSTM layers.')
-flags.DEFINE_integer('rnn_cell_size', 512,
-                     'Number of hidden units in the LSTM.')
+flags.DEFINE_integer('rnn_cell_size', 512, 'Number of hidden units in the LSTM.')
 
 # Vocabulary and embeddings
 flags.DEFINE_integer('embedding_dims', 300, 'Dimensions of embedded vector.')
@@ -75,8 +56,7 @@ flags.DEFINE_bool('normalize_embeddings', False, 'Normalize word embeddings by v
 # Optimization
 flags.DEFINE_integer('max_steps', 1000, 'Number of epochs to run.')
 flags.DEFINE_float('learning_rate', 0.0001, 'Learning rate while fine-tuning.')
-flags.DEFINE_float('learning_rate_decay_factor', 1.0,
-                   'Learning rate decay factor')
+flags.DEFINE_float('learning_rate_decay_factor', 1.0, 'Learning rate decay factor')
 
 # Regularization
 flags.DEFINE_float('max_grad_norm', 1.0, 'Clip the global gradient norm to this value.')
