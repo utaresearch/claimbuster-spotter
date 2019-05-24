dabd45ce38df45d0b2a650327ae2c0ed2fefcfdc
Kevin Meng <kevin.meng.01@gmail.com>
2019-05-24 14:46:30 -0500

    test out rmsaves

---
+++ rmsaves.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+
+
+extension_list = ['.data', '.meta', '.index']
+
+
+def list_dirs(location):
+    ret_ar = []
+    directory = os.fsencode(location)
+    for fstr in os.listdir(directory):
+        for ext in extension_list:
+            if ext in os.fsdecode(fstr):
+                ret_ar.append(os.fsdecode(fstr))
+                break
+    return ret_ar
+
+
+def main():
+    print(list_dirs(args.folder_to_clean))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder_to_clean', default='')
+    args = parser.parse_args()
+
+    main()
