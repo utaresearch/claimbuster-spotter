c92171794d074a928ff4f1d6920eb5dd3c37602d
Kevin Meng <kevin.meng.01@gmail.com>
2019-05-24 14:50:16 -0500

    filter embeddings

--- rmsaves.py
+++ rmsaves.py
@@ -9,6 +9,8 @@ def list_dirs(location):
     ret_ar = []
     directory = os.fsencode(location)
     for fstr in os.listdir(directory):
+        if not args.clear_embeddings and 'embedding_matrix' in os.fsdecode(fstr):
+            continue
         for ext in extension_list:
             if ext in os.fsdecode(fstr):
                 ret_ar.append(os.fsdecode(fstr))
@@ -23,6 +25,7 @@ def main():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--folder_to_clean', default='./output')
+    parser.add_argument('--clear_embeddings', type=bool,  default=False)
     args = parser.parse_args()
 
     main()
