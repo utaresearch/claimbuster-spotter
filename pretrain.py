4eb4a73efc17f492ddc491cfef18c2a9522c5199
Kevin Meng <kevin.meng.01@gmail.com>
2019-05-23 19:40:15 -0500

    try printing preds

--- pretrain.py
+++ pretrain.py
@@ -156,6 +156,12 @@ def main():
 
                 train_neural_network(sess, optimizer, batch_x, batch_y)
 
+                print(sess.run(y_pred, feed_dict={
+                    x: pad_seq(batch_x),
+                    x_len: [len(el) for el in batch_x],
+                    y: one_hot(batch_y)
+                }))
+
                 b_loss, b_acc = batch_stats(sess, batch_x, batch_y, cost, acc)
                 epoch_loss += b_loss
                 epoch_acc += b_acc * len(batch_y)
