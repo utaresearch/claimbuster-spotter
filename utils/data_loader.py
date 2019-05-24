9fb6fab11e9dc2ebb0592af669cce3df199cd891
Kevin Meng <kevin.meng.01@gmail.com>
2019-05-24 11:26:00 -0500

    remove exit breaker

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -75,6 +75,4 @@ class DataLoader:
         with open(FLAGS.vocab_loc if not custom_vocab_loc else custom_vocab_loc, 'rb') as f:
             vc = [x[0] for x in pickle.load(f)]
 
-        print(data)
-        exit()
         return Dataset([[vc.index(ch) for ch in x[1].split(' ')] for x in data], [int(x[0]) + 1 for x in data], FLAGS.random_state)
