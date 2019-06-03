7791ef9b5d30dd53cc4789cb1eefae9e6cce73ab
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-03 17:13:24 -0500

    no prints

--- eval.py
+++ eval.py
@@ -122,11 +122,6 @@ def main():
     data_load = DataLoader(FLAGS.custom_prc_data_loc, FLAGS.custom_vocab_loc) if FLAGS.disjoint_data else \
         DataLoader()
 
-    print(FLAGS.train_examples)
-    print(FLAGS.validation_examples)
-    print(FLAGS.test_examples)
-    print(FLAGS.total_examples)
-
     test_data = data_load.load_all_data() if FLAGS.disjoint_data else data_load.load_testing_data()
     tf.logging.info("{} testing examples".format(test_data.get_length()))
 
