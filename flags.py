3db6b47cc5681cd089063ed77fec9e370d5e9c50
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-03 16:19:12 -0500

    regularize

--- flags.py
+++ flags.py
@@ -39,7 +39,7 @@ flags.DEFINE_integer('random_state', 59, 'State of consistent pseudo-randomness'
 # Model architecture
 flags.DEFINE_bool('bidir_lstm', False, 'Whether to build a bidirectional LSTM.')
 flags.DEFINE_integer('rnn_num_layers', 1, 'Number of LSTM layers.')
-flags.DEFINE_integer('rnn_cell_size', 8, 'Number of hidden units in the LSTM.')
+flags.DEFINE_integer('rnn_cell_size', 4, 'Number of hidden units in the LSTM.')
 
 # Optimization
 flags.DEFINE_integer('max_steps', 1000, 'Number of epochs to run.')
