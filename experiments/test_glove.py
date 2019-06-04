745200c8771ab725f41d4f8f53594a46e40e1d2f
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-04 11:59:12 -0500

    new balancing factor

--- experiments/test_glove.py
+++ experiments/test_glove.py
@@ -1,5 +1,4 @@
 from gensim.models import KeyedVectors
-import numpy as np
 
 if __name__ == "__main__":
     model = KeyedVectors.load_word2vec_format("../data/word2vec/GoogleNews-vectors-negative300.bin", binary=True)
