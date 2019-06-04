c82f4fba2cbb7235dd276b4a7cd24b8dad1abcab
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-04 17:57:53 -0500

    try auto reuse

--- models/recurrent.py
+++ models/recurrent.py
@@ -20,22 +20,23 @@ class RecurrentModel:
         return yhat, loss
 
     def build_lstm(self, x, x_len, output_mask, embed, kp_emb, kp_lstm):
-        x = tf.unstack(x, axis=1)
-        for i in range(len(x)):
-            x[i] = tf.nn.embedding_lookup(embed, x[i])
-        x = tf.stack(x, axis=1)
+        with tf.variable_scope('lstm', reuse=tf.AUTO_REUSE):
+            x = tf.unstack(x, axis=1)
+            for i in range(len(x)):
+                x[i] = tf.nn.embedding_lookup(embed, x[i])
+            x = tf.stack(x, axis=1)
 
-        x = tf.nn.dropout(x, keep_prob=kp_emb)
+            x = tf.nn.dropout(x, keep_prob=kp_emb)
 
-        lstm = tf.nn.rnn_cell.MultiRNNCell([self.get_lstm(kp_lstm) for _ in range(FLAGS.rnn_num_layers)])
-        output, state = tf.nn.dynamic_rnn(cell=lstm, inputs=x, sequence_length=x_len, dtype=tf.float32)
+            lstm = tf.nn.rnn_cell.MultiRNNCell([self.get_lstm(kp_lstm) for _ in range(FLAGS.rnn_num_layers)])
+            output, state = tf.nn.dynamic_rnn(cell=lstm, inputs=x, sequence_length=x_len, dtype=tf.float32)
 
-        add_weight = tf.get_variable('post_lstm_weight', shape=(FLAGS.rnn_cell_size, FLAGS.num_classes),
-                                     initializer=tf.contrib.layers.xavier_initializer())
-        add_bias = tf.get_variable('post_lstm_bias', shape=FLAGS.num_classes,
-                                   initializer=tf.contrib.layers.xavier_initializer())
+            add_weight = tf.get_variable('post_lstm_weight', shape=(FLAGS.rnn_cell_size, FLAGS.num_classes),
+                                         initializer=tf.contrib.layers.xavier_initializer())
+            add_bias = tf.get_variable('post_lstm_bias', shape=FLAGS.num_classes,
+                                       initializer=tf.contrib.layers.xavier_initializer())
 
-        return tf.matmul(tf.boolean_mask(output, output_mask), add_weight) + add_bias
+            return tf.matmul(tf.boolean_mask(output, output_mask), add_weight) + add_bias
 
     @staticmethod
     def get_lstm(kp_lstm):
