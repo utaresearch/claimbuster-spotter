f58d3efbb4d655f3aeb59e28bb3d0cf153507ddb
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-06 14:57:42 -0500

    edit fetch binaries to run all scripts

--- fetch_binaries.sh
+++ fetch_binaries.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 python3 -m spacy download en_core_web_lg
-wget https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz \
-    -O data/word2vec/GoogleNews-vectors-negative300.bin.gz
-gunzip data/word2vec/GoogleNews-vectors-negative300.bin.gz
+
+cd data/word2vec
+./get_w2v.sh
+
+cd ../glove
+./get_glove.sh
