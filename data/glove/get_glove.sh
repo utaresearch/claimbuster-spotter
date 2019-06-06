4e55792bc378db8e1487e94150f0011edd6a462d
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-06 15:38:42 -0500

    update script to be fully functional

--- data/glove/get_glove.sh
+++ data/glove/get_glove.sh
@@ -2,6 +2,6 @@
 
 wget http://nlp.stanford.edu/data/glove.840B.300d.zip
 unzip glove.840B.300d.zip
-mv glove.840B.300d/glove.840B.300d.txt ./glove840b.txt
-rm -r glove.840B.300d
+mv glove.840B.300d.txt ./glove840b.txt
+rm -r glove.840B.300d.zip
 python3 glove_to_w2v.py
