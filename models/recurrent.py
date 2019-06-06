51c53d6dae69e8dcafad43d034fbcaed7bcd5eb0
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-05 20:18:55 -0500

    revert scopes

--- models/recurrent.py
+++ models/recurrent.py
@@ -10,16 +10,14 @@ class RecurrentModel:
         pass
 
     def construct_model(self, x, x_len, output_mask, y, embed, kp_emb, kp_lstm, adv=False):
-        var_scope_name = 'lstm{}'.format('_adv' if adv else '')
-        with tf.variable_scope(var_scope_name):
-            orig_embed, yhat = self.fprop(x, x_len, output_mask, embed, kp_emb, kp_lstm, adv=False)
-            loss = self.ce_loss(y, yhat)
+        orig_embed, yhat = self.fprop(x, x_len, output_mask, embed, kp_emb, kp_lstm, adv=False)
+        loss = self.ce_loss(y, yhat)
 
-            if adv:
-                yhat = self.fprop(x, x_len, output_mask, embed, kp_emb, kp_lstm, orig_embed, loss, adv=True)
-                loss = self.adv_loss(y, yhat)
+        if adv:
+            yhat = self.fprop(x, x_len, output_mask, embed, kp_emb, kp_lstm, orig_embed, loss, adv=True)
+            loss = self.adv_loss(y, yhat)
 
-            return yhat, loss
+        return yhat, loss
 
     def fprop(self, x, x_len, output_mask, embed, kp_emb, kp_lstm, orig_embed=None, reg_loss=None, adv=False):
         if adv:
@@ -27,33 +25,35 @@ class RecurrentModel:
         return self.build_lstm(x, x_len, output_mask, embed, kp_emb, kp_lstm, orig_embed, reg_loss, adv)
 
     def build_lstm(self, x, x_len, output_mask, embed, kp_emb, kp_lstm, orig_embed, reg_loss, adv):
-        if adv:
-            print(orig_embed, reg_loss)
-            x = apply_adversarial_perturbation(orig_embed, reg_loss)
-            tf.logging.info('Adversarial perturbations applied to {x}')
-        else:
-            x = tf.unstack(x, axis=1)
-            for i in range(len(x)):
-                x[i] = tf.nn.embedding_lookup(embed, x[i])
-            x = tf.stack(x, axis=1)
+        var_scope_name = 'lstm{}'.format('_adv' if adv else '')
+        with tf.variable_scope(var_scope_name):
+            if adv:
+                print(orig_embed, reg_loss)
+                x = apply_adversarial_perturbation(orig_embed, reg_loss)
+                tf.logging.info('Adversarial perturbations applied to {x}')
+            else:
+                x = tf.unstack(x, axis=1)
+                for i in range(len(x)):
+                    x[i] = tf.nn.embedding_lookup(embed, x[i])
+                x = tf.stack(x, axis=1)
 
-            x_embed = tf.identity(x, name='x_embed')
-            tf.logging.info('First pass of fprop() omits adversarial perturbations')
+                x_embed = tf.identity(x, name='x_embed')
+                tf.logging.info('First pass of fprop() omits adversarial perturbations')
 
-        x = tf.nn.dropout(x, keep_prob=kp_emb)
+            x = tf.nn.dropout(x, keep_prob=kp_emb)
 
-        lstm = tf.nn.rnn_cell.MultiRNNCell([self.get_lstm(kp_lstm) for _ in range(FLAGS.rnn_num_layers)])
-        output, state = tf.nn.dynamic_rnn(cell=lstm, inputs=x, sequence_length=x_len, dtype=tf.float32)
+            lstm = tf.nn.rnn_cell.MultiRNNCell([self.get_lstm(kp_lstm) for _ in range(FLAGS.rnn_num_layers)])
+            output, state = tf.nn.dynamic_rnn(cell=lstm, inputs=x, sequence_length=x_len, dtype=tf.float32)
 
-        add_weight = tf.get_variable('post_lstm_weight', shape=(FLAGS.rnn_cell_size, FLAGS.num_classes),
-                                     initializer=tf.contrib.layers.xavier_initializer())
-        add_bias = tf.get_variable('post_lstm_bias', shape=FLAGS.num_classes,
-                                   initializer=tf.zeros_initializer())
+            add_weight = tf.get_variable('post_lstm_weight', shape=(FLAGS.rnn_cell_size, FLAGS.num_classes),
+                                         initializer=tf.contrib.layers.xavier_initializer())
+            add_bias = tf.get_variable('post_lstm_bias', shape=FLAGS.num_classes,
+                                       initializer=tf.zeros_initializer())
 
-        if not adv:
-            return x_embed, tf.matmul(tf.boolean_mask(output, output_mask), add_weight) + add_bias
-        else:
-            return tf.matmul(tf.boolean_mask(output, output_mask), add_weight) + add_bias
+            if not adv:
+                return x_embed, tf.matmul(tf.boolean_mask(output, output_mask), add_weight) + add_bias
+            else:
+                return tf.matmul(tf.boolean_mask(output, output_mask), add_weight) + add_bias
 
     @staticmethod
     def get_lstm(kp_lstm):
