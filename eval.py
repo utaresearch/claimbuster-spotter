d162b600e6965888dc952dc931b4c13308d3fb89
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-08 23:54:45 -0500

    ??

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ x = tf.placeholder(tf.int32, (None, FLAGS.max_len), name='x')
 x_len = tf.placeholder(tf.int32, (None,), name='x_len')
 output_mask = tf.placeholder(tf.bool, (None, FLAGS.max_len), name='output_mask')
 y = tf.placeholder(tf.int32, (None, FLAGS.num_classes), name='y')
-kp_emb = tf.placeholder(tf.float32, name='kp_emb')
+kp_emb_1 = tf.placeholder(tf.float32, name='kp_emb_1')
 kp_lstm = tf.placeholder(tf.float32, name='kp_lstm')
 
 
@@ -35,7 +35,7 @@ def eval_stats(sess, batch_x, batch_y, cost, acc, y_pred):
             x_len: [len(el) for el in batch_x],
             output_mask: [[1 if j == len(el) - 1 else 0 for j in range(FLAGS.max_len)] for el in batch_x],
             y: one_hot(batch_y),
-            kp_emb: 1.0,
+            kp_emb_1: 1.0,
             kp_lstm: 1.0
         }
     )
@@ -46,7 +46,7 @@ def eval_stats(sess, batch_x, batch_y, cost, acc, y_pred):
             x_len: [len(el) for el in batch_x],
             output_mask: [[1 if j == len(el) - 1 else 0 for j in range(FLAGS.max_len)] for el in batch_x],
             y: one_hot(batch_y),
-            kp_emb: 1.0,
+            kp_emb_1: 1.0,
             kp_lstm: 1.0
         }
     )
@@ -57,7 +57,7 @@ def eval_stats(sess, batch_x, batch_y, cost, acc, y_pred):
             x_len: [len(el) for el in batch_x],
             output_mask: [[1 if j == len(el) - 1 else 0 for j in range(FLAGS.max_len)] for el in batch_x],
             y: one_hot(batch_y),
-            kp_emb: 1.0,
+            kp_emb_1: 1.0,
             kp_lstm: 1.0
         }
     )
@@ -66,7 +66,7 @@ def eval_stats(sess, batch_x, batch_y, cost, acc, y_pred):
 
 
 def load_model(sess, graph):
-    global x, x_len, output_mask, y, kp_emb, kp_lstm
+    global x, x_len, output_mask, y, kp_emb_1, kp_lstm
 
     def get_last_save(scan_loc):
         ret_ar = []
@@ -89,7 +89,7 @@ def load_model(sess, graph):
         x_len = graph.get_tensor_by_name('x_len:0')
         output_mask = graph.get_tensor_by_name('output_mask:0')
         y = graph.get_tensor_by_name('y:0')
-        kp_emb = graph.get_tensor_by_name('kp_emb:0')
+        kp_emb_1 = graph.get_tensor_by_name('kp_emb:0')
         kp_lstm = graph.get_tensor_by_name('kp_lstm:0')
 
         # outputs
