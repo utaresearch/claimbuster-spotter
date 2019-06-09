fbc4b6ac0f024b49a1ebd854be956482aa88d123
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-08 23:53:26 -0500

    use output dir

--- eval.py
+++ eval.py
@@ -77,12 +77,12 @@ def load_model(sess, graph):
         ret_ar.sort()
         return ret_ar[-1]
 
-    model_dir = os.path.join(FLAGS.model_dir, get_last_save(FLAGS.model_dir))
+    model_dir = os.path.join(FLAGS.output_dir, get_last_save(FLAGS.output_dir))
     tf.logging.info('Attempting to restore from {}'.format(model_dir))
 
     with graph.as_default():
         saver = tf.train.import_meta_graph(model_dir)
-        saver.restore(sess, tf.train.latest_checkpoint(FLAGS.model_dir))
+        saver.restore(sess, tf.train.latest_checkpoint(FLAGS.output_dir))
 
         # inputs
         x = graph.get_tensor_by_name('x:0')
