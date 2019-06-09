48088bea963e38974f2ee782059eaabb1733d59a
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-09 14:23:24 -0500

    revert 32 hidden units

--- flags.py
+++ flags.py
@@ -39,7 +39,7 @@ flags.DEFINE_integer('random_state', 59, 'State of pseudo-randomness')
 
 # Model architecture
 flags.DEFINE_integer('rnn_num_layers', 1, 'Number of LSTM layers.')
-flags.DEFINE_integer('rnn_cell_size', 35, 'Number of hidden units in the LSTM.')
+flags.DEFINE_integer('rnn_cell_size', 32, 'Number of hidden units in the LSTM.')
 
 # Optimization
 flags.DEFINE_integer('max_steps', 1000, 'Number of epochs to run.')
