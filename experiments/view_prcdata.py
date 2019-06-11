f2899f46053fd3074c2d9440692c94f32a2c4e2b
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-10 20:06:28 -0500

    update view files

--- experiments/view_prcdata.py
+++ experiments/view_prcdata.py
@@ -1,6 +1,11 @@
 import pickle as p
+import argparse
 
-with open('../output/prc_data.pickle', 'rb') as f:
+parser = argparse.ArgumentParser()
+parser.add_argument('--toggle', default=False, help='Toggle b/w output and disjoint')
+args = parser.parse_args()
+
+with open('../output/prc_data.pickle' if not args.toggle else '../data/disjoint_2000/prc_data.pickle', 'rb') as f:
     data = p.load(f)
 
 print(data)
--- experiments/view_vocab.py
+++ experiments/view_vocab.py
@@ -1,6 +1,11 @@
 import pickle as p
+import argparse
 
-with open('../output/vocab.pickle', 'rb') as f:
+parser = argparse.ArgumentParser()
+parser.add_argument('--toggle', default=False, help='Toggle b/w output and disjoint')
+args = parser.parse_args()
+
+with open('../output/vocab.pickle' if not args.toggle else '../data/disjoint_2000/vocab.pickle', 'rb') as f:
     data = p.load(f)
 
 print(data)
