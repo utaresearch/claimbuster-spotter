a510ca1c0a840727df990e0c2f96ad991daa0110
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-11 00:33:25 -0500

    try 128 lstm

--- flags.py
+++ flags.py
@@ -123,7 +123,7 @@ flags.DEFINE_integer('random_state', 59, 'State of pseudo-randomness')
 
 # Model architecture
 flags.DEFINE_integer('rnn_num_layers', 1, 'Number of LSTM layers.')
-flags.DEFINE_integer('rnn_cell_size', 62, 'Number of hidden units in the LSTM.')
+flags.DEFINE_integer('rnn_cell_size', 128, 'Number of hidden units in the LSTM.')
 
 # Optimization
 flags.DEFINE_integer('max_steps', 1000, 'Number of epochs to run.')
