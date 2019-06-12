1d0f700aab4fc0a1b0171b1da8fe3a31fe2469e8
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-12 14:40:13 -0500

    new updates to vocab

--- experiments/view_vocab.py
+++ experiments/view_vocab.py
@@ -8,4 +8,4 @@ args = parser.parse_args()
 with open('../output/vocab.pickle' if not args.toggle else '../data/disjoint_2000/vocab.pickle', 'rb') as f:
     data = p.load(f)
 
-print(data)
+print(sorted(data))
