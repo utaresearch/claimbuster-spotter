86d997b1862c44744724dc89a73bb6558c3b7d8f
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-16 01:34:34 -0500

    write tout

--- experiments/view_prcdata.py
+++ experiments/view_prcdata.py
@@ -13,14 +13,26 @@ vocab = data[2]
 
 print('######################### TRAIN DATA #########################')
 
-print(train_data.x)
+print([vocab[z] for z in train_data.x])
 print(train_data.y)
 
 print('######################### EVAL DATA #########################')
 
-print(eval_data.x)
+print([vocab[z] for z in eval_data.x])
 print(eval_data.y)
 
 print('######################### VOCAB #########################')
 
 print(vocab)
+
+with open('./prcdata_out.txt', 'w') as f:
+    f.write(str([vocab[z] for z in train_data.x]))
+    f.write('\n')
+    f.write(str(train_data.y))
+    f.write('\n')
+    f.write(str([vocab[z] for z in eval_data.x]))
+    f.write('\n')
+    f.write(str(eval_data.y))
+    f.write('\n')
+    f.write(str(vocab))
+    f.write('\n')
