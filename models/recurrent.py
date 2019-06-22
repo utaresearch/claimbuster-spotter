4bc2e3619522e94f48a751e4de8d6bf7ca98dc02
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-22 15:46:41 -0500

    expand last dim

--- models/recurrent.py
+++ models/recurrent.py
@@ -43,7 +43,7 @@ class RecurrentModel:
 
     @staticmethod
     def build_lstm(x, x_len, output_mask, kp_lstm):
-        x = tf.expand_dims(x)
+        x = tf.expand_dims(x, -1)
 
         if not FLAGS.bidir_lstm:
             tf.logging.info('Building uni-directional LSTM')
