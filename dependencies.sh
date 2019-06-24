cf133501d73da74fdcb2e1c1f96f05f18820e9c5
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-23 22:29:33 -0500

    do not spam pip install requis

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pip3 install -r requirements.txt --ignore-installed
+# sudo pip3 install -r requirements.txt --ignore-installed
 sudo python3 -m spacy download en_core_web_lg
 
 cd data/word2vec
