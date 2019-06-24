5a01774f37cb07fe908677ac3da3e2238f3bdec0
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-24 11:21:14 -0500

    tet

--- experiments/test_elmo.py
+++ experiments/test_elmo.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 
-pl = tf.placeholder(tf.string, (None,))
+pl = tf.placeholder(tf.int32, (None, 200, 1))
 
 elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
 # elmo_output = elmo(
