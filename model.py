b4a1cd46e3fd67b21f6004943561f1d636ace8d0
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-23 22:33:42 -0500

    test

--- model.py
+++ model.py
@@ -93,7 +93,7 @@ class ClaimBusterModel:
 
                 cb_out = tf.matmul(lstm_out, output_weights) + output_biases
 
-            for v in tf.all_variables():
+            for v in tf.global_variables(scope=None):
                 print(v.name)
 
             return (orig_embed, cb_out) if not adv else cb_out
