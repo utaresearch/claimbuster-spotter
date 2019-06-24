e09086fc27793610c2edc42ecb8816d151127237
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-24 18:31:25 -0500

    all in one file (smh)

--- demo/demo_standalone.py
+++ demo/demo_standalone.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import os
-import time
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
-from utils.data_loader import DataLoader
 from models.recurrent import RecurrentModel
 from models.embeddings import Embedding
 from sklearn.metrics import f1_score
@@ -15,7 +13,191 @@ import spacy
 import string
 from textblob import TextBlob
 from keras.preprocessing.text import text_to_word_sequence
+from sklearn.utils import shuffle
+from sklearn.utils.class_weight import compute_class_weight
+from keras.preprocessing.text import Tokenizer
+from sklearn.utils import resample
+import shutil
+import json
+import pickle
 from tqdm import tqdm
+
+
+class Dataset:
+    x = []
+    y = []
+
+    def __init__(self, x, y, random_state):
+        self.x = x
+        self.y = y
+
+        self.random_state = random_state
+        self.shuffle()
+
+    def shuffle(self):
+        self.x, self.y = shuffle(self.x, self.y, random_state=self.random_state)
+
+    def get_length(self):
+        xlen, ylen = len(self.x), len(self.y)
+        if xlen != ylen:
+            raise ValueError("size of x != size of y ({} != {})".format(xlen, ylen))
+        return xlen
+
+
+class DataLoader:
+    def __init__(self):
+        assert FLAGS.num_classes == 2 or FLAGS.num_classes == 3
+
+        self.data, self.eval_data, self.vocab = self.load_external_raw()
+
+        if FLAGS.num_classes == 2:
+            self.convert_3_to_2()
+
+        self.class_weights = self.compute_class_weights()
+        tf.logging.info('Class weights computed to be {}'.format(self.class_weights))
+
+        self.data.shuffle()
+        self.post_process_flags()
+
+    def convert_3_to_2(self):
+        self.data.y = [(1 if self.data.y[i] == 2 else 0) for i in range(len(self.data.y))]
+        self.eval_data.y = [(1 if self.eval_data.y[i] == 2 else 0) for i in range(len(self.eval_data.y))]
+
+    def compute_class_weights(self):
+        ret = compute_class_weight('balanced', [z for z in range(FLAGS.num_classes)], self.data.y)
+
+        if FLAGS.num_classes == 3:
+            ret[1] /= 4
+
+        return ret
+
+    def load_training_data(self):
+        ret = Dataset(self.data.x, self.data.y, FLAGS.random_state)
+
+        if FLAGS.sklearn_oversample:
+            classes = [[] for _ in range(FLAGS.num_classes)]
+
+            for i in range(len(ret.x)):
+                classes[ret.y[i]].append(ret.x[i])
+
+            if FLAGS.num_classes == 3:
+                maj_len = len(classes[2])
+                classes[0] = resample(classes[0], n_samples=int(maj_len * 2.75), random_state=FLAGS.random_state)
+                classes[1] = resample(classes[1], n_samples=int(maj_len * 0.90), random_state=FLAGS.random_state)
+                classes[2] = resample(classes[2], n_samples=int(maj_len * 1.50), random_state=FLAGS.random_state)
+            else:
+                pass
+                # maj_len = len(classes[0])
+                # # classes[0] = resample(classes[0], n_samples=int(maj_len), random_state=FLAGS.random_state)
+                # classes[1] = resample(classes[1], n_samples=int(maj_len * 0.40), random_state=FLAGS.random_state)
+
+            ret = Dataset([], [], random_state=FLAGS.random_state)
+            del self.data.x[:FLAGS.train_examples]
+            del self.data.y[:FLAGS.train_examples]
+
+            for lab in range(len(classes)):
+                for inp_x in classes[lab]:
+                    ret.x.append(inp_x)
+                    ret.y.append(lab)
+
+                    self.data.x.insert(0, inp_x)
+                    self.data.y.insert(0, lab)
+
+            FLAGS.total_examples += ret.get_length() - FLAGS.train_examples
+            FLAGS.train_examples = ret.get_length()
+
+        ret.shuffle()
+
+        return ret
+
+    def load_testing_data(self):
+        ret = Dataset([], [], FLAGS.random_state)
+
+        for i in range(FLAGS.test_examples):
+            ret.x.append(self.eval_data.x[i])
+            ret.y.append(self.eval_data.y[i])
+
+        return ret
+
+    def post_process_flags(self):
+        FLAGS.train_examples = self.data.get_length()
+        FLAGS.test_examples = self.eval_data.get_length()
+        FLAGS.total_examples = FLAGS.train_examples + FLAGS.test_examples
+
+    @staticmethod
+    def load_external_raw():
+        train_data, test_data, vocab = None, None, None
+
+        if FLAGS.refresh_data:
+            train_data = DataLoader.parse_json(FLAGS.raw_data_loc)
+            dj_eval_data = DataLoader.parse_json(FLAGS.raw_dj_eval_loc)
+
+            train_txt = [z[0] for z in train_data]
+            eval_txt = [z[0] for z in dj_eval_data]
+
+            train_lab = [z[1] for z in train_data]
+            eval_lab = [z[1] for z in dj_eval_data]
+
+            tf.logging.info('Loading preprocessing dependencies')
+            load_dependencies()
+
+            tf.logging.info('Processing train data')
+            train_txt, train_pos, train_sent = process_dataset(train_txt)
+            tf.logging.info('Processing eval data')
+            eval_txt, eval_pos, eval_sent = process_dataset(eval_txt)
+
+            if not FLAGS.elmo_embed:
+                tokenizer = Tokenizer()
+
+                tokenizer.fit_on_texts(np.concatenate((train_txt, eval_txt)))
+                train_seq = tokenizer.texts_to_sequences(train_txt)
+                eval_seq = tokenizer.texts_to_sequences(eval_txt)
+
+                train_data = Dataset(list(zip(train_seq, train_pos, train_sent)), train_lab, random_state=FLAGS.random_state)
+                eval_data = Dataset(list(zip(eval_seq, eval_pos, eval_sent)), eval_lab, random_state=FLAGS.random_state)
+                vocab = tokenizer.word_index
+            else:
+                train_txt = [z.split(' ') for z in train_txt]
+                eval_txt = [z.split(' ') for z in eval_txt]
+
+                train_data = Dataset(list(zip(train_txt, train_pos, train_sent)), train_lab, random_state=FLAGS.random_state)
+                eval_data = Dataset(list(zip(eval_txt, eval_pos, eval_sent)), eval_lab, random_state=FLAGS.random_state)
+
+            with open(FLAGS.prc_data_loc, 'wb') as f:
+                pickle.dump((train_data, eval_data, vocab), f)
+            tf.logging.info('Refreshed data, successfully dumped at {}'.format(FLAGS.prc_data_loc))
+
+            if os.path.exists(FLAGS.output_dir):
+                shutil.rmtree(FLAGS.output_dir)
+            os.mkdir(FLAGS.output_dir)
+        else:
+            tf.logging.info('Restoring data from {}'.format(FLAGS.prc_data_loc))
+            with open(FLAGS.prc_data_loc, 'rb') as f:
+                train_data, eval_data, vocab = pickle.load(f)
+
+        if not FLAGS.elmo_embed:
+            assert vocab is not None
+
+        return train_data, eval_data, vocab
+
+    @staticmethod
+    def parse_json(json_loc):
+        with open(json_loc) as f:
+            temp_data = json.load(f)
+
+        dl = []
+        labels = [0, 0, 0]
+
+        for el in temp_data:
+            lab = int(el["label"]) + 1
+            txt = el["text"]
+
+            labels[lab] += 1
+            dl.append([txt, lab])
+
+        print('{}: {}'.format(json_loc, labels))
+        return dl
+
 
 nlp = None
 cont = None
