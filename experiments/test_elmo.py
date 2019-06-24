b69d0114d1757a79ad3f7ebcb93f30424fc2599a
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-24 11:15:42 -0500

    print all outputs

--- experiments/test_elmo.py
+++ experiments/test_elmo.py
@@ -1,9 +1,9 @@
 import tensorflow_hub as hub
 
 elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
-embeddings = elmo(
+elmo_output = elmo(
     ["the cat is on the mat", "dogs are in the fog"],
     signature="default",
     as_dict=True)["elmo"]
 
-print(embeddings)
+print(elmo_output)
