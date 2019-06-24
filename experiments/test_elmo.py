829138b509cba464a7d5445ba503fda07eeb3f02
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-24 11:34:40 -0500

    test elmo output

--- experiments/test_elmo.py
+++ experiments/test_elmo.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 
-x = tf.placeholder(tf.string, (None, None))
-x_len = tf.placeholder(tf.int32, (None,))
+pl = tf.placeholder(tf.string, (None, ))
 
 elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
+# elmo_output = elmo(
+#     ["the cat is on the mat", "dogs are in the fog"],
+#     signature="default",
+#     as_dict=True)
 
-tokens_input = [["the", "cat", "is", "on", "the", "mat"],
-                ["dogs", "are", "in", "the", "fog", ""]]
-tokens_length = [200 for _ in tokens_input]
 elmo_output = elmo(
-    inputs={"tokens": x, "sequence_len": x_len},
-    signature="tokens",
+    ["the cat is on the mat", "dogs are in the fog"],
+    signature="default",
     as_dict=True)
 
 print(elmo_output)
