ad72e9945fd970c1bba4bffa84226f798d0bd503
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-26 14:47:25 -0500

    input example filtering

--- model.py
+++ model.py
@@ -229,8 +229,7 @@ class ClaimBusterModel:
         return np.sum(run_loss), run_acc, np.argmax(run_pred, axis=1)
 
     def get_preds(self, sess, sentence_tuple):
-        print(type(sentence_tuple[0][0]))
-        x_nl, x_pos, x_sent = ([z] for z in sentence_tuple)
+        x_nl, x_pos, x_sent = ([z] if 'InputExample' not in z[0].__name__ else z for z in sentence_tuple)
         feed_dict = self.get_feed_dict(x_nl, x_pos, x_sent, ver='test')
 
         return sess.run(self.y_pred, feed_dict=feed_dict)
