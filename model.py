4bd7aac79be9c3602db465114b8c3671879580b3
Kevin Meng <kevin.meng.01@gmail.com>
2019-06-26 14:45:04 -0500

    type of st1

--- model.py
+++ model.py
@@ -229,8 +229,7 @@ class ClaimBusterModel:
         return np.sum(run_loss), run_acc, np.argmax(run_pred, axis=1)
 
     def get_preds(self, sess, sentence_tuple):
-        print(sentence_tuple)
-
+        print(type(sentence_tuple[0]))
         x_nl, x_pos, x_sent = ([z] for z in sentence_tuple)
         feed_dict = self.get_feed_dict(x_nl, x_pos, x_sent, ver='test')
 
