f70cfd1f45ed12bd9c50e651fafcff301b89ec9b
Kevin Meng <kevin.meng.01@gmail.com>
2019-07-15 01:28:42 -0500

    print init vars

--- models/bert_model.py
+++ models/bert_model.py
@@ -308,6 +308,7 @@ def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
         name_to_variable[name] = var
 
     init_vars = tf.train.list_variables(init_checkpoint)
+    print(init_vars)
 
     assignment_map = collections.OrderedDict()
     for x in init_vars:
