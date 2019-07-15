ebc37893e6fc3b6fdbddc651535a63c64fa0c1b7
Kevin Meng <kevin.meng.01@gmail.com>
2019-07-15 01:01:44 -0500

    does this work?

--- train.py
+++ train.py
@@ -26,6 +26,9 @@ def main():
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         sess.run(tf.global_variables_initializer())
 
+        if not FLAGS.use_bert_hub:
+            sess.run(cb_model.init_bert_pretrain_op)
+
         start = time.time()
         epochs_trav = 0
 
