2e95ea769c6d00ee8ba01993e8a6ba351f1aa0a2
Kevin Meng <kevin.meng.01@gmail.com>
2019-07-18 23:20:20 -0500

    color

--- model.py
+++ model.py
@@ -336,7 +336,7 @@ class ClaimBusterModel:
             ret_ar = []
             directory = os.fsencode(scan_loc)
             for fstr in os.listdir(directory):
-                if os.path.isdir(os.path.join(scan_loc, os.fsdecode(fstr)))
+                if os.path.isdir(os.path.join(scan_loc, os.fsdecode(fstr))):
                     ret_ar.append(os.fsdecode(fstr))
             ret_ar.sort()
             return os.path.join(scan_loc, ret_ar[-1]) + '/'
