1967f58e63fc706c4963155c5a63b2b75da2341c
Kevin Meng <kevin.meng.01@gmail.com>
2019-07-20 18:29:36 -0500

    test sentence tok

---
+++ experiments/xlnet_tokenization.py
@@ -0,0 +1,15 @@
+import sentencepiece as spm
+import sys
+sys.path.append('..')
+from flags import FLAGS
+from models.xlnet.prepro_utils import preprocess_text, encode_ids
+
+# some code omitted here...
+# initialize FLAGS
+
+text = "An input text string."
+
+sp_model = spm.SentencePieceProcessor()
+sp_model.Load(os.path.join(FLAGS.xlnet_model_loc, 'spiece.model'))
+text = preprocess_text(text, lower=False)
+ids = encode_ids(sp_model, text)
