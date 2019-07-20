8d205f4319da7093461266cc0a31e6c6e6e745df
Kevin Meng <kevin.meng.01@gmail.com>
2019-07-20 01:33:03 -0500

    it creates xlnet

--- data/get_xlnet.sh
+++ data/get_xlnet.sh
@@ -3,5 +3,5 @@
 wget https://storage.googleapis.com/xlnet/released_models/cased_L-12_H-768_A-12.zip
 unzip cased_L-12_H-768_A-12.zip
 
-mv cased_L-12_H-768_A-12 xlnet_pretrain
+mv xlnet_cased_L-12_H-768_A-12 xlnet_pretrain
 rm cased_L-12_H-768_A-12.zip
