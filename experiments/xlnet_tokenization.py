51f63f76bdcab65bb65c794174dd7cab95f0154f
Kevin Meng <kevin.meng.01@gmail.com>
2019-07-20 19:25:17 -0500

    print something

--- experiments/xlnet_tokenization.py
+++ experiments/xlnet_tokenization.py
@@ -14,3 +14,5 @@ sp_model = spm.SentencePieceProcessor()
 sp_model.Load(os.path.join('..', FLAGS.xlnet_model_loc, 'spiece.model'))
 text = preprocess_text(text, lower=False)
 ids = encode_ids(sp_model, text)
+
+print(text, ids)
