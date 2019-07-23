01f7591e1b3c1f8a8eb865ff8a7450ba8e3abd2c
Kevin Meng <kevin.meng.01@gmail.com>
2019-07-23 13:09:16 -0500

    use the same variable scope... this might actually be the problem

--- models/xlnet/xlnet.py
+++ models/xlnet/xlnet.py
@@ -216,7 +216,7 @@ class XLNetModel(object):
             inp_q=inp_q)
         tfm_args.update(input_args)
 
-        with tf.variable_scope("model", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope("model/", reuse=tf.AUTO_REUSE):
             (self.output, self.new_mems, self.lookup_table
              ) = modeling.transformer_xl(**tfm_args)
 
@@ -238,7 +238,7 @@ class XLNetModel(object):
         xlnet_config = self.xlnet_config
         run_config = self.run_config
 
-        with tf.variable_scope("model", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope("model/", reuse=tf.AUTO_REUSE):
             summary = modeling.summarize_sequence(
                 summary_type=summary_type,
                 hidden=self.output,
