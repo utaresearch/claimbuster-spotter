8ebeea62df118d18fa152e5ec7ccae10f66fa675
Kevin Meng <kevin.meng.01@gmail.com>
2019-07-23 18:27:11 -0500

    move xlnet opt to the bottom

--- model.py
+++ model.py
@@ -111,81 +111,6 @@ class ClaimBusterModel:
 
         return opt
 
-    @staticmethod
-    def build_optimizer_xlnet(total_loss, adv):
-        global_step = tf.train.get_or_create_global_step()
-
-        # increase the learning rate linearly
-        if FLAGS.warmup_steps > 0:
-            warmup_lr = tf.cast(global_step, tf.float32) / tf.cast(FLAGS.warmup_steps, tf.float32) * FLAGS.lr
-        else:
-            warmup_lr = 0.0
-
-        max_steps = FLAGS.pretrain_steps if not adv else FLAGS.advtrain_steps
-
-        # decay the learning rate
-        if FLAGS.decay_method == "poly":
-            decay_lr = tf.train.polynomial_decay(
-                FLAGS.lr,
-                global_step=global_step - FLAGS.warmup_steps,
-                decay_steps=max_steps - FLAGS.warmup_steps,
-                end_learning_rate=FLAGS.lr * FLAGS.min_lr_ratio)
-        elif FLAGS.decay_method == "cos":
-            decay_lr = tf.train.cosine_decay(
-                FLAGS.lr,
-                global_step=global_step - FLAGS.warmup_steps,
-                decay_steps=max_steps - FLAGS.warmup_steps,
-                alpha=FLAGS.min_lr_ratio)
-        else:
-            raise ValueError(FLAGS.decay_method)
-
-        learning_rate = tf.where(global_step < FLAGS.warmup_steps,
-                                 warmup_lr, decay_lr)
-
-        if FLAGS.weight_decay == 0:
-            optimizer = tf.train.AdamOptimizer(
-                learning_rate=learning_rate,
-                epsilon=FLAGS.adam_epsilon)
-        else:
-            optimizer = AdamWeightDecayOptimizer(
-                learning_rate=learning_rate,
-                epsilon=FLAGS.adam_epsilon,
-                exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"],
-                weight_decay_rate=FLAGS.weight_decay)
-
-        grads_and_vars = optimizer.compute_gradients(total_loss)
-
-        gradients, variables = zip(*grads_and_vars)
-        clipped, gnorm = tf.clip_by_global_norm(gradients, FLAGS.clip)
-
-        if getattr(FLAGS, "lr_layer_decay_rate", 1.0) != 1.0:
-            n_layer = 0
-            for i in range(len(clipped)):
-                m = re.search(r"model//transformer//layer_(\d+?)//", variables[i].name)
-                if not m: continue
-                n_layer = max(n_layer, int(m.group(1)) + 1)
-
-            print('successfully successfully successfully successfully successfully {}'.format(n_layer))
-
-            for i in range(len(clipped)):
-                for l in range(n_layer):
-                    if "model//transformer//layer_{}//".format(l) in variables[i].name:
-                        abs_rate = FLAGS.lr_layer_decay_rate ** (n_layer - 1 - l)
-                        clipped[i] *= abs_rate
-                        tf.logging.info("Apply mult {:.4f} to layer-{} grad of {}".format(
-                            abs_rate, l, variables[i].name))
-                        break
-
-        train_op = optimizer.apply_gradients(
-            zip(clipped, variables), global_step=global_step)
-
-        # Manually increment `global_step` for AdamWeightDecayOptimizer
-        if FLAGS.weight_decay > 0:
-            new_global_step = global_step + 1
-            train_op = tf.group(train_op, [global_step.assign(new_global_step)])
-
-        return train_op, learning_rate, gnorm
-
     def construct_model(self):
         orig_embed, logits = self.fprop()
         self.trainable_variables = self.select_train_vars()
@@ -440,3 +365,79 @@ class ClaimBusterModel:
             init_checkpoint = get_last_save(dr)
             am, _ = get_assignment_map_from_checkpoint(tf.trainable_variables())
             tf.train.init_from_checkpoint(init_checkpoint, am)
+
+    # Unused method
+    @staticmethod
+    def build_optimizer_xlnet(total_loss, adv):
+        global_step = tf.train.get_or_create_global_step()
+
+        # increase the learning rate linearly
+        if FLAGS.warmup_steps > 0:
+            warmup_lr = tf.cast(global_step, tf.float32) / tf.cast(FLAGS.warmup_steps, tf.float32) * FLAGS.lr
+        else:
+            warmup_lr = 0.0
+
+        max_steps = FLAGS.pretrain_steps if not adv else FLAGS.advtrain_steps
+
+        # decay the learning rate
+        if FLAGS.decay_method == "poly":
+            decay_lr = tf.train.polynomial_decay(
+                FLAGS.lr,
+                global_step=global_step - FLAGS.warmup_steps,
+                decay_steps=max_steps - FLAGS.warmup_steps,
+                end_learning_rate=FLAGS.lr * FLAGS.min_lr_ratio)
+        elif FLAGS.decay_method == "cos":
+            decay_lr = tf.train.cosine_decay(
+                FLAGS.lr,
+                global_step=global_step - FLAGS.warmup_steps,
+                decay_steps=max_steps - FLAGS.warmup_steps,
+                alpha=FLAGS.min_lr_ratio)
+        else:
+            raise ValueError(FLAGS.decay_method)
+
+        learning_rate = tf.where(global_step < FLAGS.warmup_steps,
+                                 warmup_lr, decay_lr)
+
+        if FLAGS.weight_decay == 0:
+            optimizer = tf.train.AdamOptimizer(
+                learning_rate=learning_rate,
+                epsilon=FLAGS.adam_epsilon)
+        else:
+            optimizer = AdamWeightDecayOptimizer(
+                learning_rate=learning_rate,
+                epsilon=FLAGS.adam_epsilon,
+                exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"],
+                weight_decay_rate=FLAGS.weight_decay)
+
+        grads_and_vars = optimizer.compute_gradients(total_loss)
+
+        gradients, variables = zip(*grads_and_vars)
+        clipped, gnorm = tf.clip_by_global_norm(gradients, FLAGS.clip)
+
+        if getattr(FLAGS, "lr_layer_decay_rate", 1.0) != 1.0:
+            n_layer = 0
+            for i in range(len(clipped)):
+                m = re.search(r"model//transformer//layer_(\d+?)//", variables[i].name)
+                if not m: continue
+                n_layer = max(n_layer, int(m.group(1)) + 1)
+
+            print('successfully successfully successfully successfully successfully {}'.format(n_layer))
+
+            for i in range(len(clipped)):
+                for l in range(n_layer):
+                    if "model//transformer//layer_{}//".format(l) in variables[i].name:
+                        abs_rate = FLAGS.lr_layer_decay_rate ** (n_layer - 1 - l)
+                        clipped[i] *= abs_rate
+                        tf.logging.info("Apply mult {:.4f} to layer-{} grad of {}".format(
+                            abs_rate, l, variables[i].name))
+                        break
+
+        train_op = optimizer.apply_gradients(
+            zip(clipped, variables), global_step=global_step)
+
+        # Manually increment `global_step` for AdamWeightDecayOptimizer
+        if FLAGS.weight_decay > 0:
+            new_global_step = global_step + 1
+            train_op = tf.group(train_op, [global_step.assign(new_global_step)])
+
+        return train_op, learning_rate, gnorm
