79bde437e04549b430e7eb8a6ee3ec8ee35a8c69
Kevin Meng <kevin.meng.01@gmail.com>
2019-08-12 15:57:57 -0500

    update dependency loading

--- dependencies.sh
+++ dependencies.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # sudo pip3 install -r requirements.txt
-sudo python3 -m spacy download en_core_web_lg
+# sudo python3 -m spacy download en_core_web_lg
 
 cd data/word2vec
 ./get_w2v.sh
+
+cd ../
 ./get_bert.sh
+
+# ./get_xlnet.sh
