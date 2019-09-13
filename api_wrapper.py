46825fe65e6bf0ebb946b2b781ceda533d1c822f
Kevin Meng <kevin.meng.01@gmail.com>
2019-09-12 23:05:30 -0500

    refactor; add an API file

---
+++ api_wrapper.py
@@ -0,0 +1,57 @@
+from bert import run_classifier
+import numpy as np
+import os
+from model import ClaimBusterModel
+from utils.data_loader import DataLoader
+from utils import transformations as transf
+from flags import FLAGS
+import tensorflow as tf
+
+
+class ClaimBusterAPI:
+    def __init__(self):
+        self.return_strings = ['Non-factual statement', 'Unimportant factual statement', 'Salient factual statement']
+        self.tokenizer = DataLoader.create_tokenizer_from_hub_module()
+
+        tf.logging.set_verbosity(tf.logging.ERROR)
+        os.environ['CUDA_VISIBLE_DEVICES'] = ','.join([str(z) for z in FLAGS.gpu])
+
+        data_load = DataLoader()
+        self.vocab = data_load.vocab
+
+        transf.load_dependencies()
+
+        graph = tf.get_default_graph()
+        self.cb_model = ClaimBusterModel(data_load.vocab, data_load.class_weights, restore=True, adv=False)
+        self.cb_model.load_model(graph, train=False)
+
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+
+        self.sess = tf.Session(graph=graph, config=config)
+        self.sess.run(tf.global_variables_initializer())
+        self.cb_model.load_model(self.sess, graph)
+
+    def prc_sentence(self, sentence, vocab):
+        sentence, pos, sent = self.extract_info(sentence, vocab)
+
+        input_examples = [run_classifier.InputExample(guid="", text_a=sentence, text_b=None, label=0)]
+        input_features = run_classifier.convert_examples_to_features(input_examples,
+                                                                     [z for z in range(FLAGS.num_classes)],
+                                                                     FLAGS.max_len, self.tokenizer)
+
+        return input_features, pos, sent
+
+    def subscribe_query(self):
+        print('Enter a sentence to process')
+        sentence_tuple = self.prc_sentence(input().strip('\n\r\t '), self.vocab)
+        return self.cb_model.get_preds(self.sess, sentence_tuple)
+
+    @staticmethod
+    def extract_info(sentence, vocab):
+        sentence = transf.transform_sentence_complete(sentence)
+
+        sent = transf.get_sentiment(sentence)
+        pos = transf.process_sentence_full_tags(sentence)
+
+        return sentence, pos, sent
--- demo.py
+++ demo.py
@@ -1,72 +1,13 @@
-from bert import run_classifier
 import numpy as np
-import os
-from model import ClaimBusterModel
-from utils.data_loader import DataLoader
-from utils import transformations as transf
-from flags import FLAGS
-import tensorflow as tf
-
-return_strings = ['Non-factual statement', 'Unimportant factual statement', 'Salient factual statement']
-tokenizer = DataLoader.create_tokenizer_from_hub_module()
-
-
-def extract_info(sentence, vocab):
-    sentence = transf.transform_sentence_complete(sentence)
-
-    sent = transf.get_sentiment(sentence)
-    pos = transf.process_sentence_full_tags(sentence)
-
-    return sentence, pos, sent
-
-
-def prc_sentence(sentence, vocab):
-    global tokenizer
-
-    sentence, pos, sent = extract_info(sentence, vocab)
-
-    input_examples = [run_classifier.InputExample(guid="", text_a=sentence, text_b=None, label=0)]
-    input_features = run_classifier.convert_examples_to_features(input_examples,
-                                                                 [z for z in range(FLAGS.num_classes)],
-                                                                 FLAGS.max_len, tokenizer)
-
-    return input_features, pos, sent
-
-
-def subscribe_query(sess, cb_model, vocab):
-    print('Enter a sentence to process')
-    sentence_tuple = prc_sentence(input().strip('\n\r\t '), vocab)
-    return cb_model.get_preds(sess, sentence_tuple)
-
-
-def main():
-    os.environ['CUDA_VISIBLE_DEVICES'] = ','.join([str(z) for z in FLAGS.gpu])
-
-    data_load = DataLoader()
-    vocab = data_load.vocab
-
-    transf.load_dependencies()
-
-    graph = tf.get_default_graph()
-    cb_model = ClaimBusterModel(data_load.vocab, data_load.class_weights, restore=True, adv=False)
-    cb_model.load_model(graph, train=False)
-
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-
-    with tf.Session(graph=graph, config=config) as sess:
-        sess.run(tf.global_variables_initializer())
-        cb_model.load_model(sess, graph)
-
-        while True:
-            res = subscribe_query(sess, cb_model, vocab)
-            idx = np.argmax(res, axis=1)
-
-            print(res)
-
-            print('{} with probability {}'.format(np.array(return_strings)[idx][0], res[0][idx][0]))
-
+from api_wrapper import ClaimBusterAPI
 
 if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.ERROR)
-    main()
+    api = ClaimBusterAPI()
+
+    while True:
+        res = api.subscribe_query()
+        idx = np.argmax(res, axis=1)
+
+        print(res)
+
+        print('{} with probability {}'.format(np.array(api.return_strings)[idx][0], res[0][idx][0]))
