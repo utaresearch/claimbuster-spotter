c5772772cef8bf2a6c57107f197b4b17bcd81fb5
Kevin Meng <kevin.meng.01@gmail.com>
2019-10-11 22:49:45 -0500

    add CB API calls

---
+++ distrib_analysis.py
@@ -0,0 +1,43 @@
+import numpy as np
+import nltk.data
+import re
+import matplotlib.pyplot as plt
+import requests
+from urllib import quoteplus
+import argparse
+
+from api_wrapper import ClaimBusterAPI
+
+
+def main():
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    fp = open("./data/ClintonTrump.txt")
+    rawdata = fp.read()
+    data = rawdata.split('\n')
+    data = [x for x in data if not x == '']
+
+    sentence_list = []
+    for x in data:
+        interm = [re.sub(r'[A-Z0-9][A-Z0-9. ]*: ', '', x) for x in tokenizer.tokenize(x)]
+        if len(sentence_list) == 0:
+            sentence_list = interm
+        else:
+            sentence_list = np.concatenate([sentence_list, interm])
+
+    # Get claimbuster information
+    if args.advcb:
+        api = ClaimBusterAPI()
+        scores_advcb = sorted([api.direct_sentence_query(x)[2] for x in sentence_list])
+        print('\n'.join([str(x) for x in scores_advcb]))
+
+    if args.svmcb:
+        scores_svmcb = [requests.get("https://idir.uta.edu/factchecker/score_text/{}".format(quoteplus(x))).json()["results"][0]["score"] for x in sentence_list]
+        print('\n'.join([str(x) for x in scores_advcb]))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--advcb', True)
+    parser.add_argument('--svmcb', True)
+    args = parser.parse_args()
+
+    main()
