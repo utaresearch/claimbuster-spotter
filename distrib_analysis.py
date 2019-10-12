ae984fe95a79ff56f281d738f3af6bd5f3d54bff
Kevin Meng <kevin.meng.01@gmail.com>
2019-10-11 22:55:09 -0500

    new updates

--- distrib_analysis.py
+++ distrib_analysis.py
@@ -4,7 +4,7 @@ import re
 import matplotlib.pyplot as plt
 import requests
 from urllib.parse import quote
-import argparse
+import sys
 
 from api_wrapper import ClaimBusterAPI
 
@@ -25,19 +25,17 @@ def main():
             sentence_list = np.concatenate([sentence_list, interm])
 
     # Get claimbuster information
-    if args.advcb:
+    if sys.argv[1] == 0 or sys.argv[1] == 2:
         api = ClaimBusterAPI()
         scores_advcb = sorted([api.direct_sentence_query(x)[2] for x in sentence_list])
         print('\n'.join([str(x) for x in scores_advcb]))
 
-    if args.svmcb:
+    if sys.argv[1] == 1 or sys.argv[1] == 2:
         scores_svmcb = [requests.get("https://idir.uta.edu/factchecker/score_text/{}".format(quote(x))).json()["results"][0]["score"] for x in sentence_list]
         print('\n'.join([str(x) for x in scores_advcb]))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-advcb', True)
-    parser.add_argument('-svmcb', True)
-    args = parser.parse_args()
+    if len(sys.argv != 2):
+        raise Exception('argv: 0 if advcb, 1 if svmcb, 2 if both')
 
     main()
