c777d83f31459d5fee653c15cf722043798f45e8
Kevin Meng <kevin.meng.01@gmail.com>
2019-10-13 02:00:29 -0500

    export everything >:(

--- clef.py
+++ clef.py
@@ -12,15 +12,4 @@ def get_score(text):
 df = pd.read_csv("data/CT19-T1-Test.csv")
 
 df['score'] = df.apply(lambda x: get_score(x['text']), axis=1)
-
-df.to_csv("clef_out.csv", columns=['line_number', 'score'], header=False, index=None)
-
-fout = open('clef_out_clean.csv', 'w')
-
-with open('clef_out.csv', 'r') as fin:
-    for line in fin:
-        data = line.split(',')
-        data[1] = str(round(float(data[1]), 4))
-        fout.write('\t'.join(data) + '\n')
-
-fout.close()
+df.to_csv("clef_out.csv", index=None)
