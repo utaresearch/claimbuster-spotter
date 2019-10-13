c69fe09e7bd2747edb0ef34d11e31e42ef66822c
Kevin Meng <kevin.meng.01@gmail.com>
2019-10-12 23:27:22 -0500

    only output score and line number

--- clef.py
+++ clef.py
@@ -13,4 +13,4 @@ df = pd.read_csv("data/CT19-T1-Test.csv")
 
 df['score'] = df.apply(lambda x: get_score(x['text']), axis=1)
 
-df.to_csv("clef_out.csv", index=None)
+df.to_csv("clef_out.csv", columns=['line_number', 'score'], index=None)
