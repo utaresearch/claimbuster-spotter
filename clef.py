a576b09a7a011bec54ebd2b1ef18e655734e636e
Kevin Meng <kevin.meng.01@gmail.com>
2019-10-13 00:40:04 -0500

    updates to clef.py

--- clef.py
+++ clef.py
@@ -14,3 +14,15 @@ df = pd.read_csv("data/CT19-T1-Test.csv")
 df['score'] = df.apply(lambda x: get_score(x['text']), axis=1)
 
 df.to_csv("clef_out.csv", columns=['line_number', 'score'], index=None)
+
+fout = open('clef_gen.csv', 'w')
+
+with open('clef_out.csv', 'r') as fin:
+    for line in fin:
+        data = line.split(',')
+        data[1] = str(round(float(data[1]), 4))
+        fout.write('\t'.join(data))
+
+import os
+os.remove('clef_out.csv')
+fout.close()
