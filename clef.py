fcfb00e18ad8a5440e9bae6b042ca60e208ddeef
Kevin Meng <kevin.meng.01@gmail.com>
2019-10-13 00:47:49 -0500

    no head

--- clef.py
+++ clef.py
@@ -13,7 +13,7 @@ df = pd.read_csv("data/CT19-T1-Test.csv")
 
 df['score'] = df.apply(lambda x: get_score(x['text']), axis=1)
 
-df.to_csv("clef_out.csv", columns=['line_number', 'score'], index=None)
+df.to_csv("clef_out.csv", columns=['line_number', 'score'], header=False, index=None)
 
 fout = open('clef_gen.csv', 'w')
 
