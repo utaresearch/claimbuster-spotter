ed555fd1712e325f3353b1ad831437bae534ce34
Kevin Meng <kevin.meng.01@gmail.com>
2019-10-13 01:43:22 -0500

    re-write

--- clef.py
+++ clef.py
@@ -15,7 +15,7 @@ df['score'] = df.apply(lambda x: get_score(x['text']), axis=1)
 
 df.to_csv("clef_out.csv", columns=['line_number', 'score'], header=False, index=None)
 
-fout = open('clef_gen.csv', 'w')
+fout = open('clef_out_clean.csv', 'w')
 
 with open('clef_out.csv', 'r') as fin:
     for line in fin:
@@ -23,6 +23,4 @@ with open('clef_out.csv', 'r') as fin:
         data[1] = str(round(float(data[1]), 4))
         fout.write('\t'.join(data) + '\n')
 
-import os
-os.remove('clef_out.csv')
 fout.close()
