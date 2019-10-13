d11d6ac55e6e71cf2d72f3f84af6348cc0859e72
Kevin Meng <kevin.meng.01@gmail.com>
2019-10-12 21:30:58 -0500

    update compute ndcg module

--- experiments/compute_ndcg.py
+++ experiments/compute_ndcg.py
@@ -6,8 +6,11 @@ def compute_dcg_term(i, labels, ver=1):
         else ((1 << labels[i - 1]) - 1) / math.log2(i + 1)
 
 
-# Precondition: labels is sorted in descending order, according to the magnitude of scores
-def compute_ndcg(labels):
+# Precondition: for each index i, scores[i] corresponds with labels[i]
+def compute_ndcg(scores, labels):
+    combined = sorted([(scores[i], labels[i]) for i in range(len(scores))], reverse=True)
+    labels = [x[1] for x in combined]
+
     selver = 0
 
     dcg = sum([compute_dcg_term(i, labels, ver=selver) for i in range(1, len(labels) + 1, 1)])
@@ -18,4 +21,5 @@ def compute_ndcg(labels):
 
 
 if __name__ == "__main__":
-    print(compute_ndcg([3,2,3,0,1,2]))
+    print(compute_ndcg([5, 4, 3, 2, 1, 0], [3, 2, 3, 0, 1, 2]))
+    print(compute_ndcg([5, 3, 4, 0, 1, 2], [3, 3, 2, 2, 1, 0]))
