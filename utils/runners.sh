aca7c7a6412e49673bf4ba5c3ffcaf91133aa31a
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-05 23:48:12 -0600

    final update before refactor

--- utils/runners.sh
+++ utils/runners.sh
@@ -13,11 +13,16 @@ nohup sh -c "python3 eval.py --gpu=3 --cb_model_dir=/adversarial-claimspotting/o
 
 nohup sh -c "python3 eval.py --gpu=1 --cb_model_dir=/adversarial-claimspotting/output-adv-6/023"&>asdf6.txt&
 
-
+python3 clef.py --cb_model_dir=reg_clef --use_clef_data=True --gpu=2 --num_classes=2
+cp clef_out.csv ../clef2019-factchecking-task1/scorer/data/
+python3 fatma_prc.py
 python main.py --gold_file_path="data/g1.tsv, data/g2.tsv, data/g3.tsv, data/g4.tsv, data/g5.tsv, data/g6.tsv, data/g7.tsv" --pred_file_path="data/p1.tsv, data/p2.tsv, data/p3.tsv, data/p4.tsv, data/p5.tsv, data/p6.tsv, data/p7.tsv"
 
 
-nohup sh -c "python3 pretrain.py --cb_model_dir=reg_clef --gpu=0 --use_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=False"&>nohup.out&
-nohup sh -c "python3 advtrain.py --cb_model_dir=adv_clef --gpu=1 --use_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=False"&>nohup1.out&
-nohup sh -c "python3 pretrain.py --cb_model_dir=reg_ours_clef --gpu=2 --use_clef_data=True --combine_ours_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=False"&>nohup2.out&
-nohup sh -c "python3 advtrain.py --cb_model_dir=adv_ours_clef --gpu=3 --use_clef_data=True --combine_ours_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=False"&>nohup3.out&
+nohup sh -c "python3 pretrain.py --cb_model_dir=reg_clef --gpu=0 --use_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=True"&>nohup.out&
+nohup sh -c "python3 advtrain.py --cb_model_dir=adv_clef --gpu=1 --use_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=True"&>nohup1.out&
+nohup sh -c "python3 pretrain.py --cb_model_dir=reg_ours_clef --gpu=2 --use_clef_data=True --combine_ours_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=True"&>nohup2.out&
+nohup sh -c "python3 advtrain.py --cb_model_dir=adv_ours_clef --gpu=3 --use_clef_data=True --combine_ours_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=True"&>nohup3.out&
+
+
+python3 pretrain.py --cb_model_dir=reg_ours_clef --gpu=2 --use_clef_data=True --combine_ours_clef_data=True --weight_classes_loss=True -num_classes=2 --restore_and_continue=False
