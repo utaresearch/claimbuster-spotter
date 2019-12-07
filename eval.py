1f694612745866584dd176b82f32d1a28c321fff
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-06 23:11:27 -0600

    test new eval

--- eval.py
+++ eval.py
@@ -1,90 +1,97 @@
-import numpy as np
 import math
+import time
 import os
 from tqdm import tqdm
+from shutil import rmtree
 from utils.data_loader import DataLoader
 from model import ClaimBusterModel
-from sklearn.metrics import f1_score, classification_report
-from flags import FLAGS
-from utils.compute_ndcg import compute_ndcg
+from flags import FLAGS, print_flags
+from absl import logging
 import tensorflow as tf
+import numpy as np
+from model import ClaimBusterModel
+
+K = tf.keras
 
 
 def main():
     os.environ['CUDA_VISIBLE_DEVICES'] = ','.join([str(z) for z in FLAGS.gpu])
     os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'True'
 
-    tf.logging.info("Loading dataset")
+    print_flags()
+
+    if os.path.isdir(FLAGS.tb_dir):
+        rmtree(FLAGS.tb_dir)
+    if os.path.isdir(FLAGS.cb_model_dir) and not FLAGS.restore_and_continue:
+        print('Continue? (y/n) You will overwrite the contents of FLAGS.cb_model_dir ({})'.format(FLAGS.cb_model_dir))
+        inp = input().strip('\r\n\t')
+        if inp.lower() == 'y':
+            rmtree(FLAGS.cb_model_dir)
+        else:
+            print('Exiting...')
+            exit()
+    if not os.path.isdir(FLAGS.cb_model_dir) and FLAGS.restore_and_continue:
+        raise Exception('Cannot restore from non-existent folder: {}'.format(FLAGS.cb_model_dir))
+
+    logging.info("Loading dataset")
     data_load = DataLoader()
 
+    train_data = data_load.load_training_data()
     test_data = data_load.load_testing_data()
-    tf.logging.info("{} testing examples".format(test_data.get_length()))
 
-    graph = tf.get_default_graph()
-    cb_model = ClaimBusterModel(data_load.vocab, data_load.class_weights, restore=True, adv=True)
-    cb_model.load_model(graph)
+    logging.info("{} training examples".format(train_data.get_length()))
+    logging.info("{} validation examples".format(test_data.get_length()))
 
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
+    model = ClaimBusterModel()
+    dataset_train = tf.data.Dataset.from_tensor_slices(([x[0] for x in train_data.x], train_data.y)).shuffle(
+        buffer_size=train_data.get_length()).batch(FLAGS.batch_size)
+    dataset_test = tf.data.Dataset.from_tensor_slices(([x[0] for x in test_data.x], test_data.y)).shuffle(
+        buffer_size=test_data.get_length()).batch(FLAGS.batch_size)
 
-    with tf.Session(graph=graph, config=config) as sess:
-        sess.run(tf.global_variables_initializer())
+    logging.info("Warming up...")
+    model.call(K.layers.Input(shape=(FLAGS.max_len,), dtype='int32'))
+    logging.info("Starting training...")
 
-        n_batches = math.ceil(float(FLAGS.test_examples) / float(FLAGS.batch_size))
-        n_samples = 0
-        eval_loss, eval_loss_adv, eval_acc, eval_acc_adv = 0.0, 0.0, 0.0, 0.0
+    epochs_trav = 0
+    for epoch in range(FLAGS.pretrain_steps):
+        epochs_trav += 1
+        epoch_loss, epoch_acc = 0, 0
+        start = time.time()
 
-        y_all = []
-        pred_all = []
-        pred_all_adv = []
-        cw_score_all = []
-        cw_score_all_adv = []
+        pbar = tqdm(total=math.ceil(len(train_data.y) / FLAGS.batch_size))
+        for x_id, y in dataset_train:
+            train_batch_loss, train_batch_acc = model.train_on_batch(x_id, y)
+            epoch_loss += train_batch_loss
+            epoch_acc += train_batch_acc * np.shape(y)[0]
+            pbar.update(1)
 
-        for i in tqdm(range(n_batches)):
-            batch_x, batch_y = cb_model.get_batch(i, test_data, ver='test')
+        epoch_loss /= train_data.get_length()
+        epoch_acc /= train_data.get_length()
 
-            b_loss, b_loss_adv, b_acc, b_acc_adv, b_pred, b_pred_adv, b_pred_raw, b_pred_adv_raw = \
-                cb_model.stats_from_run(sess, batch_x, batch_y, adv=True)
-            if b_loss == 0 and b_acc == 0 and b_pred == 0:
-                continue
+        if epoch % FLAGS.stat_print_interval == 0:
+            log_string = 'Epoch {:>3} Loss: {:>7.4} Acc: {:>7.4f}% '.format(epoch + 1, epoch_loss, epoch_acc * 100)
 
-            eval_loss += b_loss
-            eval_loss_adv += b_loss_adv
-            eval_acc += b_acc * len(batch_y)
-            eval_acc_adv += b_acc_adv * len(batch_y)
-            n_samples += len(batch_y)
+            if test_data.get_length() > 0:
+                val_loss, val_acc = 0, 0
 
-            y_all = np.concatenate((y_all, batch_y))
-            pred_all = np.concatenate((pred_all, b_pred))
-            pred_all_adv = np.concatenate((pred_all_adv, b_pred_adv))
-            cw_score_all = np.concatenate((cw_score_all, [x[FLAGS.num_classes - 1] for x in b_pred_raw]))
-            cw_score_all_adv = np.concatenate((cw_score_all_adv, [x[FLAGS.num_classes - 1] for x in b_pred_adv_raw]))
-            
-        f1score = f1_score(y_all, pred_all, average='weighted')
-        f1score_adv = f1_score(y_all, pred_all_adv, average='weighted')
+                for x_id, y in dataset_test:
+                    val_batch_loss, val_batch_acc = model.stats_on_batch(x_id, y)
+                    val_loss += val_batch_loss
+                    val_acc += val_batch_acc * np.shape(y)[0]
 
-        ndcg = compute_ndcg(cw_score_all, y_all)
-        ndcg_adv = compute_ndcg(cw_score_all_adv, y_all)
+                val_loss /= test_data.get_length()
+                val_acc /= test_data.get_length()
 
-        eval_loss /= n_samples
-        eval_loss_adv /= n_samples
-        eval_acc /= n_samples
-        eval_acc_adv /= n_samples
+                log_string += 'Dev Loss: {:>7.4f} Dev Acc: {:>7.4f} '.format(val_loss, val_acc)
 
-        print('Labels:', y_all)
-        print('Predic:', pred_all)
+            log_string += '({:3.3f} sec/epoch)'.format((time.time() - start) / epochs_trav)
 
-        target_names = (['NFS', 'UFS', 'CFS'] if FLAGS.num_classes == 3 else ['NFS/UFS', 'CFS'])
+            print(log_string)
 
-        tf.logging.info('Final stats     | Loss: {:>7.4} Acc: {:>7.4f}% F1: {:>.4f} nDCG: {:>.4f}'.format(
-            eval_loss, eval_acc * 100, f1score, ndcg))
-        print(classification_report(y_all, pred_all, target_names=target_names, digits=4))
-
-        tf.logging.info('Final adv stats | Loss: {:>7.4} Acc: {:>7.4f}% F1: {:>.4f} nDCG: {:>.4f}'.format(
-            eval_loss_adv, eval_acc_adv * 100, f1score_adv, ndcg_adv))
-        print(classification_report(y_all, pred_all_adv, target_names=target_names, digits=4))
+            start = time.time()
+            epochs_trav = 0
 
 
 if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.INFO)
+    logging.set_verbosity(logging.INFO)
     main()
