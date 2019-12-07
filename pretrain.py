e7290e52e99cf5c8c746eea031e560507f8ea4aa
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-06 21:56:28 -0600

    do not warm up

--- pretrain.py
+++ pretrain.py
@@ -45,7 +45,6 @@ def main():
     model = ClaimBusterModel()
     dataset = tf.data.Dataset.from_tensor_slices(([x[0] for x in train_data.x], train_data.y)).shuffle(
         buffer_size=train_data.get_length()).batch(FLAGS.batch_size)
-    model.build((None, FLAGS.max_len))
     model.warm_up(dataset)
 
     logging.info("Starting training...")
