61856fcf3865add37ae1766d63a157b95e9d372f
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-06 23:50:44 -0600

    new

--- pretrain.py
+++ pretrain.py
@@ -60,7 +60,7 @@ def main():
         if any('.ckpt' in x for x in os.listdir(FLAGS.cb_model_dir)):
             load_location = FLAGS.cb_model_dir
         else:
-            folders = [x for x in os.listdir(FLAGS.cb_model_dir) if os.path.isdir(x)]
+            folders = [x for x in os.listdir(FLAGS.cb_model_dir) if os.path.isdir(os.path.join(FLAGS.cb_model_dir, x))]
             print(os.listdir(FLAGS.cb_model_dir), folders)
             load_location = sorted(folders)[-1]
 
