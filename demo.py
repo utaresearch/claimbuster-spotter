a1e0bbf29648a54daa75ffc83225683b606ae392
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-07 12:08:28 -0600

    add all demo tests

--- demo.py
+++ demo.py
@@ -4,6 +4,26 @@ from api_wrapper import ClaimBusterAPI
 if __name__ == '__main__':
     api = ClaimBusterAPI()
 
+    print('--- Batch Sentence Query ---')
+
+    sentence_list = [
+        'Donald Trump is the 45th President of the United States',
+        'I really like cheese',
+        'McDonalds earns $10 billion dollars each minute'
+    ]
+
+    print(sentence_list)
+    print(api.batch_sentence_query())
+    print('Test OK')
+
+    print('--- Single Sentence Query ---')
+
+    print(sentence_list[0])
+    print(api.single_sentence_query(sentence_list[0]))
+    print('Test OK')
+
+    print('--- Command Line Query ---')
+
     while True:
         res = api.subscribe_cmdline_query()[0]
         idx = np.argmax(res)
