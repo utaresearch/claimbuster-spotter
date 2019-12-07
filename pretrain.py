da06b8ecdc3f232c4ea4e3fc7f833587149d34f4
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-06 23:16:35 -0600

    two part dbug

--- pretrain.py
+++ pretrain.py
@@ -49,6 +49,9 @@ def main():
         buffer_size=test_data.get_length()).batch(FLAGS.batch_size)
 
     logging.info("Warming up...")
+
+    model.call(K.layers.Input(shape=(FLAGS.max_len,), dtype='int32'))
+
     kmodel = K.models.Model(K.layers.Input(shape=(FLAGS.max_len,), dtype='int32'),
                             model.call(K.layers.Input(shape=(FLAGS.max_len,), dtype='int32')))
     logging.info("Starting training...")
