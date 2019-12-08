23c801d817d493addbd44dd24946f01afcb023bd
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-08 15:49:34 -0600

    remove vocab + word2vec

--- dependencies.sh
+++ dependencies.sh
@@ -3,8 +3,8 @@
 # sudo pip3 install -r requirements.txt
 # sudo python3 -m spacy download en_core_web_lg
 
-cd data/word2vec
-./get_w2v.sh
+# cd data/word2vec
+# ./get_w2v.sh
 
 cd ../
 ./get_bert.sh
