9b82615f3f2a16dc0cb7339a66e986015a633ada
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-08 16:33:34 -0600

    modification to dependency loading

--- utils/transformations.py
+++ utils/transformations.py
@@ -389,16 +389,12 @@ def process_sentence_ner_spacy(sentence):
 def load_dependencies():
     global nlp, cont, embed_obj
 
-    print("Loading NLTK Dependencies...")
-    nltk.download('punkt')
-    nltk.download('averaged_perceptron_tagger')
-    nltk.download('tagsets')
-    nltk.download('stopwords')
     if FLAGS.ner_spacy:
         print("Loading Spacy NER Tagger...")
         nlp = spacy.load("en_core_web_lg")
         print("Tagger loaded.")
-    print("NLTK dependencies Loaded.")
+
+    print("Dependencies Loaded.")
 
 
 def load_deps_dummy():
