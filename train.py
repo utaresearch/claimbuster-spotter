75c5e59c7b72f8754df0ce31599c52b2d5fccf9c
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-09 16:13:34 -0600

    test_data

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ def main():
 
     dataset_train = tf.data.Dataset.from_tensor_slices(([x[0] for x in train_data.x], [x[1] for x in train_data.x], train_data.y)).shuffle(
         buffer_size=train_data.get_length()).batch(FLAGS.batch_size_reg if not FLAGS.adv_train else FLAGS.batch_size_adv)
-    dataset_test = tf.data.Dataset.from_tensor_slices(([x[0] for x in train_data.x], [x[1] for x in test_data.x], test_data.y)).shuffle(
+    dataset_test = tf.data.Dataset.from_tensor_slices(([x[0] for x in test_data.x], [x[1] for x in test_data.x], test_data.y)).shuffle(
         buffer_size=test_data.get_length()).batch(FLAGS.batch_size_reg if not FLAGS.adv_train else FLAGS.batch_size_adv)
 
     logging.info("Warming up...")
