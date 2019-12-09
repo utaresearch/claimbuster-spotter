4ac7cb50c0e5f529e463ed1a1c84d532897445fd
Kevin Meng <kevin.meng.01@gmail.com>
2019-12-09 16:05:09 -0600

    test

--- train.py
+++ train.py
@@ -42,6 +42,11 @@ def main():
     logging.info("{} training examples".format(train_data.get_length()))
     logging.info("{} validation examples".format(test_data.get_length()))
 
+    for crap in train_data:
+        if len(crap[0] != 200) or len(crap[1] != 2):
+            print(len(crap[0]))
+            print(len(crap[1]))
+
     dataset_train = tf.data.Dataset.from_tensor_slices((train_data.x, train_data.y)).shuffle(
         buffer_size=train_data.get_length()).batch(FLAGS.batch_size_reg if not FLAGS.adv_train else FLAGS.batch_size_adv)
     dataset_test = tf.data.Dataset.from_tensor_slices((test_data.x, test_data.y)).shuffle(
