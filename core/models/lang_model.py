93fe9972ad84057d87594e772aa571b87e40e0b7
Kevin Meng <kevin.meng.01@gmail.com>
2020-01-07 01:09:56 -0600

    albert unknown

--- core/models/lang_model.py
+++ core/models/lang_model.py
@@ -12,21 +12,18 @@ class LanguageModel:
 
     @staticmethod
     def build_transformer():  # independent of tfm type
-        if FLAGS.cs_tfm_type == 'albert':
-            return LanguageModel.test_albert()
-
         bert_params = bert2.params_from_pretrained_ckpt(FLAGS.cs_model_loc)
         bert_params.hidden_dropout = 1 - FLAGS.cs_kp_tfm_hidden
         bert_params.attention_dropout = 1 - FLAGS.cs_kp_tfm_atten
         return bert2.BertModelLayer.from_params(bert_params, name=FLAGS.cs_tfm_type)
 
-    @staticmethod
-    def test_albert():
-        albert_module = hub.Module(
-            "https://tfhub.dev/google/albert_base/2",
-            trainable=True)
-        albert_inputs = dict(
-            input_ids=K.layers.Input(shape=(FLAGS.cs_max_len,), dtype='int32')
-        )
-        albert_outputs = albert_module(albert_inputs, signature="tokens", as_dict=True)
-        return albert_outputs["pooled_output"]
+    # @staticmethod
+    # def test_albert():
+    #     albert_module = hub.Module(
+    #         "https://tfhub.dev/google/albert_base/2",
+    #         trainable=True)
+    #     albert_inputs = dict(
+    #         input_ids=K.layers.Input(shape=(FLAGS.cs_max_len,), dtype='int32')
+    #     )
+    #     albert_outputs = albert_module(albert_inputs, signature="tokens", as_dict=True)
+    #     return albert_outputs["pooled_output"]
