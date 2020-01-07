52d3144d6c48194145c98c90d7284970af8f7917
Kevin Meng <kevin.meng.01@gmail.com>
2020-01-07 01:05:56 -0600

    test?

--- core/models/lang_model.py
+++ core/models/lang_model.py
@@ -1,5 +1,9 @@
 from . import bert2
 from ..utils.flags import FLAGS
+import tensorflow_hub as hub
+import tensorflow as tf
+
+K = tf.keras
 
 
 class LanguageModel:
@@ -12,3 +16,14 @@ class LanguageModel:
         bert_params.hidden_dropout = 1 - FLAGS.cs_kp_tfm_hidden
         bert_params.attention_dropout = 1 - FLAGS.cs_kp_tfm_atten
         return bert2.BertModelLayer.from_params(bert_params, name=FLAGS.cs_tfm_type)
+
+    @staticmethod
+    def test_albert():
+        albert_module = hub.Module(
+            "https://tfhub.dev/google/albert_base/2",
+            trainable=True)
+        albert_inputs = dict(
+            input_ids=K.layers.Input(shape=(FLAGS.cs_max_len,), dtype='int32')
+        )
+        albert_outputs = albert_module(albert_inputs, signature="tokens", as_dict=True)
+        return albert_outputs["pooled_output"]
