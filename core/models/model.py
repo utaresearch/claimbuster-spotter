6efd4be6dc91f16aa30408549f278f9f4ecf6317
Kevin Meng <kevin.meng.01@gmail.com>
2020-03-09 20:01:24 -0500

    test fix

--- core/models/model.py
+++ core/models/model.py
@@ -94,7 +94,7 @@ class ClaimSpotterLayer(K.layers.Layer):
         get_embedding = -1 if 'get_embedding' not in kwargs else kwargs.get('get_embedding')
 
         if get_embedding == -1:
-            bert_output = self.bert_model(x_id, training=training)
+            bert_output = self.bert_model(x_id, training=training, perturb=perturb)
         else:
             orig_embed, bert_output = self.bert_model(x_id, perturb=perturb, get_embedding=get_embedding, training=training)
 
