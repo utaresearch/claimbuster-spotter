8b867eab544d798414fb4b1df0b0ebe8353fa769
Kevin Meng <kevin.meng.01@gmail.com>
2020-06-23 21:48:55 -0500

    add timing component

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,7 @@
 import numpy as np
 import string
 import random
+import time
 from core.api.api_wrapper import ClaimSpotterAPI
 
 
@@ -12,5 +13,10 @@ if __name__ == '__main__':
     api = ClaimSpotterAPI()
 
     sentence_list = [generate_sentence() for _ in range(10000)]
-    print(api.batch_sentence_query(sentence_list))
+
+    gstart = time.time()
+    api.batch_sentence_query(sentence_list)
+    gend = time.time()
+
+    print('{} tweets processed in {} seconds: {} tweets per second'.format(len(sentence_list), gend - gstart, len(sentence_list) / (gend - gstart)))
     print('Test OK')
