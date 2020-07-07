c5e9ce51756a89d2b9f06eaa17eb23d26f9e0e0c
Jacob Devasier <jdevasier12@gmail.com>
2020-07-07 12:39:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -21,16 +21,36 @@ def get_user_input(r, input_text, k="input_text"):
 
     return ""
 
-@app.route("/score/sentences/", methods=["POST"])
+@app.route("/score/batches/", methods=["POST"])
 async def score_sentences(request):
+    """
+    Returns the scores of the batches of text provided.
+
+    Parameters
+    ----------
+    paragraphs : List[string]
+        Input batches to be scored.
+    
+    Returns
+    -------
+    Each sentence from each batch scored with the claimspotter model.
+
+    <Response>
+        Returns a response object with the body containing:
+            List[List[Dict]]
+            {
+                `text` : string
+                `index`: string
+                `score`: float
+                `result`: string
+            }
+    """
     paragraphs = get_user_input(request, "", k="paragraphs")
 
     tokenized_paragraphs = [sent_tokenize(sentences) for sentences in paragraphs]
     scored_paragraphs = [api.batch_sentence_query(sentences) for sentences in tokenized_paragraphs]
 
-    print(tokenized_paragraphs, scored_paragraphs)
     results = [[{"text":tokenized_paragraphs[i][j], "index":j, "score":scored_paragraphs[i][j][1], "result":api.return_strings[argmax(scored_paragraphs[i][j])]} for j in range(len(tokenized_paragraphs[i]))] for i in range(len(tokenized_paragraphs))]
-    # results = [{"text":sentences[i], "index":i, "score":scores[i][1], "result":api.return_strings[argmax(scores[i])]} for i in range(len(sentences))]
     return json(results)
 
 
