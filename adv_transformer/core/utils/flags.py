7c0dcfa017300fdb7d614541e747b7bc35b40aba
Kevin Meng <kevin.meng.01@gmail.com>
2020-11-24 16:31:23 -0600

    revamp flags

--- adv_transformer/core/utils/flags.py
+++ adv_transformer/core/utils/flags.py
@@ -40,8 +40,6 @@ flags.DEFINE_string('cs_reg_train_file', 'train.json', 'Data file to use for non
 flags.DEFINE_string('cs_reg_test_file', 'test.json', 'Data file to use for testing post non-KFold training.')
 flags.DEFINE_string('cs_tb_dir', './tb_logs', 'Tensorboard location')
 
-flags.DEFINE_bool('cs_adv_train', False, 'Use adversarial training?')
-
 # Data
 flags.DEFINE_string('cs_data_file_encoding', None, 'Default encoding for data. Default value is None.')
 flags.DEFINE_bool('cs_use_clef_data', False, 'Use CLEF data, rather than ClaimBuster')
@@ -51,50 +49,43 @@ flags.DEFINE_bool('cs_remove_stopwords', False, 'Remove stop words (e.g. the, a,
 flags.DEFINE_bool('cs_sklearn_oversample', False, 'Oversample underrepresented classes with sklearn')
 flags.DEFINE_bool('cs_weight_classes_loss', False, 'Weight classes in CE loss function')
 flags.DEFINE_bool('cs_custom_preprc', True, 'Use custom pre-processing')
+flags.DEFINE_integer('cs_random_state', 59, 'State of pseudo-randomness')
 
-# Eval
+flags.DEFINE_integer('cs_num_classes', 2, 'Number of classes for classification (2 combines NFS and UFS)')
+flags.DEFINE_bool('cs_alt_two_class_combo', False, 'Combine CFS and UFS instead when num_classes=2.')
+
 flags.DEFINE_integer('cs_stat_print_interval', 1, 'Numbers of epochs before stats are printed again')
 flags.DEFINE_integer('cs_model_save_interval', 1, 'Numbers of epochs before model is saved again')
 
-# Data v2
-flags.DEFINE_integer('cs_random_state', 59, 'State of pseudo-randomness')
+# Training
+flags.DEFINE_integer('cs_k_fold', 4, 'Number of folds for k-fold cross validation')
+flags.DEFINE_bool('cs_restore_and_continue', False, 'Restore previous training session and continue')
+flags.DEFINE_integer('cs_batch_size_reg', 24, 'Size of the batch.')
+flags.DEFINE_integer('cs_batch_size_adv', 24, 'Size of the batch when adversarial training.')
 
-# Model architecture
-flags.DEFINE_integer('cs_cls_hidden', 0, 'Size of hidden classification layer')
-flags.DEFINE_float('cs_kp_cls', 0.7, 'Keep probability of dropout in FC')
-
-# Optimization
 flags.DEFINE_integer('cs_train_steps', 15, 'Number of epochs to run.')
 flags.DEFINE_float('cs_lr', 5e-5, 'Learning rate during optimization.')
-
-# Regularization
 flags.DEFINE_float('cs_l2_reg_coeff', 0, 'If val > 0, use L2 Regularization on weights in graph')
 
-# Adversarial and virtual adversarial training parameters.
+# Adversarial and virtual adversarial training parameters
+flags.DEFINE_bool('cs_adv_train', False, 'Use adversarial training?')
 flags.DEFINE_integer('cs_perturb_id', 0, "Index in [('pos', 'seg', 'tok'), ('pos', 'seg'), ('pos', 'tok'), ('seg', 'tok'), ('pos',), ('seg',), ('tok',)] to perturb")
 flags.DEFINE_integer('cs_adv_type', 0, '0 for AT, 1 for VAT')
 flags.DEFINE_float('cs_lambda', 0.25, 'Coefficient of adversarial loss')
 flags.DEFINE_bool('cs_combine_reg_adv_loss', True, 'Add loss of regular and adversarial loss during training')
 flags.DEFINE_float('cs_perturb_norm_length', 2.0, 'Norm length of adversarial perturbation')
 
-# Output stats
-flags.DEFINE_integer('cs_num_classes', 2, 'Number of classes for classification (2 combines NFS and UFS)')
-flags.DEFINE_bool('cs_alt_two_class_combo', False, 'Combine CFS and UFS instead when num_classes=2.')
-
-# Transformer
+# Model Architecture
 flags.DEFINE_string('cs_tfm_type', 'bert-base-uncased', 'Type of transformer; see https://huggingface.co/transformers/pretrained_models.html')
+flags.DEFINE_string('cs_pooling_strategy', 'first', 'How to pool: using average of all steps or [CLS]')
 flags.DEFINE_bool('cs_tfm_ft_embed', False, 'Train transf embedding layer')
 flags.DEFINE_bool('cs_tfm_ft_pooler', True, 'Train transf pooler layer')
 flags.DEFINE_integer('cs_tfm_ft_enc_layers', 2, 'Last n encoding layers are marked as trainable')
 flags.DEFINE_float('cs_kp_tfm_attn', 0.8, 'Keep probability of attention dropout in Transformer')
 flags.DEFINE_float('cs_kp_tfm_hidden', 0.8, 'Keep probability of hidden dropout in Transformer')
 
-# Training
-flags.DEFINE_integer('cs_k_fold', 4, 'Number of folds for k-fold cross validation')
-flags.DEFINE_bool('cs_adam', True, 'Adam or RMSProp if False')
-flags.DEFINE_bool('cs_restore_and_continue', False, 'Restore previous training session and continue')
-flags.DEFINE_integer('cs_batch_size_reg', 24, 'Size of the batch.')
-flags.DEFINE_integer('cs_batch_size_adv', 24, 'Size of the batch when adversarial training.')
+flags.DEFINE_integer('cs_cls_hidden', 0, 'Size of hidden classification layer')
+flags.DEFINE_float('cs_kp_cls', 0.7, 'Keep probability of dropout in FC')
 
 
 def clean_argv(inp):
@@ -126,6 +117,8 @@ flags.DEFINE_string('cs_prc_clef_loc', '{}/all_clef_data.pickle'.format(FLAGS.cs
 
 FLAGS.cs_prc_data_loc = FLAGS.cs_prc_data_loc[:-7] + '_{}'.format(FLAGS.cs_tfm_type) + '.pickle'
 
+# Sanity Checks
+assert FLAGS.cs_pooling_strategy in ['mean', 'first']
 assert FLAGS.cs_num_classes == 2, 'FLAGS.cs_num_classes must be 2: 3 class comparisons are deprecated.'
 assert FLAGS.cs_stat_print_interval % FLAGS.cs_model_save_interval == 0
 
