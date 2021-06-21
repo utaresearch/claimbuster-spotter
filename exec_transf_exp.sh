d3652d540c3bf6b9c79d6803a33d8e271676a765
Kevin Meng <kevin.meng.01@gmail.com>
2021-06-20 20:32:00 -0400

    use CUDA_VISIBLE_DEVICES in the call itself

--- exec_transf_exp.sh
+++ exec_transf_exp.sh
@@ -1,4 +1,4 @@
 for i in {0..3}
 do
-  nohup sh -c "python3 -m adv_transformer.train --cs_model_dir=output_bert --cs_gpu=$i --cs_tfm_type=bert-base-uncased --cs_train_steps=15"&>nohup_bert_$i.out&
+  CUDA_VISIBLE_DEVICES=$i nohup sh -c "python3 -m adv_transformer.train --cs_model_dir=output_bert_$i --cs_tfm_type=bert-base-uncased --cs_train_steps=15"&>nohup_bert_$i.out&
 done
