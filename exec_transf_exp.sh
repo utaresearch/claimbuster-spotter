44084be8fcfb32b5f6954680d16aa02f103116c8
Kevin Meng <kevin.meng.01@gmail.com>
2021-06-28 18:43:56 -0400

    reduce training steps

--- exec_transf_exp.sh
+++ exec_transf_exp.sh
@@ -1,5 +1,5 @@
-for i in {0..3}
+for i in {1..3}
 do
 #  CUDA_VISIBLE_DEVICES=$i nohup sh -c "python3 -m adv_transformer.train --cs_model_dir=output_bert_$i --cs_tfm_type=bert-base-uncased --cs_train_steps=10 --cs_perturb_id=6 --cs_adv_train=True"&>nohup_bert_$i.out&
-  CUDA_VISIBLE_DEVICES=$i nohup sh -c "python3 -m adv_transformer.train --cs_model_dir=output_distilbert_$i --cs_tfm_type=distilbert-base-uncased --cs_train_steps=10 --cs_adv_train=True --cs_perturb_id=6"&>nohup_distilbert_$i.out&
+  CUDA_VISIBLE_DEVICES=$i nohup sh -c "python3 -m adv_transformer.train --cs_model_dir=output_distilbert_$i --cs_tfm_type=distilbert-base-uncased --cs_train_steps=7 --cs_adv_train=True --cs_perturb_id=6"&>nohup_distilbert_$i.out&
 done
